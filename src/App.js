5fee061a2fe20f59ce3e2a838d6a8c66d22b847c
Denis Borodin <borodindk@gmail.com>
2019-08-20 19:46:30 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map(t => (
+        <figure>
+          <h2>{t.name}</h2>
+          <p>{t.description}</p>
+          <p>{t.location.address}</p>
+        </figure>
+      ))}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: "volna",
+    name: "Волна",
+    description: `Описание`,
+    images: [],
+    location: {
+      address: "Москва, Проспект мира 10",
+      lat: 0,
+      lng: 0
+    },
+    dates: {
+      build: '1991-01-11',
+      drop: '2005'
+    }
+  }
+]
